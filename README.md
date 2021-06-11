93e614b40f6cca77e3370d402ccd660296125bb0
hotbagels <81516937+hotbagels@users.noreply.github.com>
2021-06-11 18:18:02 -0400

    Timed class changes worked as planned, made a few minor adjustments and added some more comments. Added a better readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# daily-event-tracker
-Daily planner that is color coded to visually track the day flying by.
+# Daily Event Planner
+
+## A color coded planner that visually tracks the day flying by.
+
+- In terms of personal challenge, this project challeneged me on a few concepts. It was my first time adapting the Moment.js library in a useful senario. It gave me a good refrence for the ways to display and control the data provided. This was also the first time I got to work through dynamic time-based class changes, as well as an upgrade in the amount of different local storage items to set up.
+
+- This web app used HTML, CSS, Javascript, JQuery, Moment.JS, Bootstarp, Google Fonts, & Font Awesome.
+
+- As far as usability, lets go through what this bad boy can do:
+
+  - With a nice calming fade into the day, you're greeted with an up-to-date clock at the top keeping track of time every minute.
+  - A work days worth of time plotted out below, with the ability to edit and insert tasks that you have finished or plan to finish thoughtout the day.
+  - Those tasks can be saved on the page, and you can come and go from the page as you please with those tasks staying where you left them.
+  - If you're done with the days tasks or wants a fresh start, a button under the clock lets you wipe clean and start again like a brand new day.
+
+[Click Here](https://shaneconwell.github.io/daily-event-tracker/) to check out the app!
+
+![Screenshot of an awesome daily task planner](./assets/images/workday-screenshot.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,18 @@
 var saveButton = $(".saveBtn");
 var timeDisplayEl = $('#time-display');
-var rightNow
 var resetButtonEl = $('#resetBtn');
 
+var rightNow
 
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar
-
 function displayTime() {
     rightNow = moment().format('MMM DD, YYYY [at] hh:mm a');
     timeDisplayEl.text(rightNow);
     return;
 }
 
+// button saves text in the description field  as a value onto local Storage with its time parent being the key. 
 saveButton.on("click", function () {
     var time = $(this).parent().attr("id")
     var value = $(this).siblings(".description").val();
@@ -20,28 +20,26 @@ saveButton.on("click", function () {
 });
 
 // Timeblocks are color coded to indicate whether it is in the past, present, or future
-
 var currentTime = moment().hours();
-console.log(currentTime);
 $(".time-block").each(function () {
-    var thisHour = parseInt($(this).attr("id").replace(/hour-/, ''));
+    var thisHour = parseInt($(this).attr("id").replace(/hour-/,''));
     console.log(thisHour);
     if (thisHour < currentTime) {
         $(this).removeClass("future")
         $(this).removeClass("present");
         $(this).addClass("past");
     } else if (thisHour === currentTime) {
-        $(this).addClass("past");
+        $(this).removeClass("past");
         $(this).removeClass("future")
         $(this).addClass("present");
     } else {
         $(this).removeClass("present");
-        $(this).addClass("past");
+        $(this).removeClass("past");
         $(this).addClass("future");
     }
 });
 
-
+// gets any values saved in local storage and appends those saved values onto the child of their matching key
 $("#hour-9").children(2).val(localStorage.getItem("hour-9"));
 $("#hour-10").children(2).val(localStorage.getItem("hour-10"));
 $("#hour-11").children(2).val(localStorage.getItem("hour-11"));
@@ -52,11 +50,12 @@ $("#hour-15").children(2).val(localStorage.getItem("hour-15"));
 $("#hour-16").children(2).val(localStorage.getItem("hour-16"));
 $("#hour-17").children(2).val(localStorage.getItem("hour-17"));
 
+// reset button added to clear storage, possibly at the start of a new day
 resetButtonEl.on("click", function () {
     localStorage.clear();
     location.reload();
 });
 
-
+// interval running to  update the displayed time every second
 setInterval(displayTime, 1000);
 localStorage.setItem
